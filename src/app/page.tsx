7a5d843ff26a986384c9a77728ca9f376a802d99
Edward King <king8three@gmailcom>
2025-08-06 16:05:06 -0400

    work on welcome pg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,30 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background text-foreground">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        <h1 className="text-5xl font-extrabold tracking-tight text-foreground sm:text-[5rem]">
+          Welcome <span className="text-[hsl(280,100%,70%)]">to</span> Script Manager
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-card p-4 text-card-foreground hover:bg-secondary border border-border"
             href="https://create.t3.gg/en/usage/first-steps"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <h3 className="text-2xl font-bold">Start from Scratch?→</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+              Create, name and store your script. More reliable than our brains
             </div>
           </Link>
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-card p-4 text-card-foreground hover:bg-secondary border border-border"
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <h3 className="text-2xl font-bold">Find Your Script →</h3>
             <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+              Welcome back. Find your securely stored scripts here
             </div>
           </Link>
         </div>
