fd852a352c51e5c9744f1c01514d54e73d02063b
Edward King <king8three@gmailcom>
2025-08-06 23:01:16 -0400

    updated routing and home styling

---
+++ src/app/components/search/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function SearchPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background text-foreground">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-foreground sm:text-[5rem]">
+          Search <span className="text-[hsl(280,100%,70%)]">Here</span> 
+        </h1>
+       
+      </div>
+    </main>
+  );
+}
