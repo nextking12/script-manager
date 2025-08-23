91c10b6aafae77d1a091b8d3e66f27c870b81ccb
Edward King <king8three@gmailcom>
2025-08-23 13:50:08 -0400

    controller good for now

--- script_manager_back/src/main/java/com/example/script_manager_back/ScriptController.java
+++ script_manager_back/src/main/java/com/example/script_manager_back/ScriptController.java
@@ -1,6 +1,8 @@
 package com.example.script_manager_back;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,24 +12,56 @@ public class ScriptController {
 
     private final ScriptService scriptService;
 
+    public ScriptController(ScriptService scriptService){
+        this.scriptService = scriptService;
+    }
+
     @PostMapping("/create") // create new script
     public ResponseEntity<Script> createScript(@RequestBody Script script) {
         Script createdScript = scriptService.createScript(script);
         return new ResponseEntity<>(createdScript, HttpStatus.CREATED);
     }
 
-    @GetMapping("/search/{name}")
+    @GetMapping("/search/name/{name}") // search by name
     public ResponseEntity<Script> getScriptByName(@PathVariable String name) {
         return scriptService.getScriptByName(name)
                 .map(script -> new ResponseEntity<>(script, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @GetMapping("/search/{language}")
+    @GetMapping("/search/language/{language}") // search by language
     public ResponseEntity<Script> getScriptByLanguage(@PathVariable String language) {
         return scriptService.getScriptByLanguage(language)
                 .map(script -> new ResponseEntity<>(script, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping // get all scripts
+    public ResponseEntity<List<Script>> getAllScripts() {
+        List<Script> scripts = scriptService.getAllScripts();
+        return new ResponseEntity<>(scripts, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{name}")
+    public ResponseEntity<Script> updateScript(@PathVariable String name, @RequestBody Script script) {
+        try {
+
+            Script updatedScript = scriptService.updateScript(name, script);
+            return new ResponseEntity<>(updatedScript, HttpStatus.OK);
+        } catch (RuntimeException e) {
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/byName/{name}")
+    public ResponseEntity<Void> deleteScriptByName(@PathVariable String name) {
+        try {
+            scriptService.deleteScriptByName(name);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
